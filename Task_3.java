3f461756724b727e45c1655453d36f98df2e4bdf
tmrqt <timerkhan12@gmail.com>
2023-04-14 09:08:25 +0300

    done

---
+++ Task_3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+
+public class Task_3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n1 = input.nextInt();
+        int n2 = input.nextInt();
+        char operation = input.next().charAt(0);
+        int result = calc(n1, n2, operation);
+        System.out.println("Результат операции: " + result);
+    }
+    public static int calc(int num1, int num2, char operation){
+        int result;
+        switch (operation){
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1/num2;
+                break;
+            default:
+                System.out.println("Операция не распознана. Повторите ввод.");
+                result = 0;
+        }
+        return result;
+    }
+}
+
+
